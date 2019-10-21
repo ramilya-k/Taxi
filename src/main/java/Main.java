0f36955c35e6a01f3dbfad0521f2e537ed5c6463
Рамиля <zizuozishou9>
2019-10-22 01:19:39 +0300

    fixes #5

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        TaxiOrderService taxiService = new TaxiOrderService();
+        int price = taxiService.price(7);
+        System.out.println(price);
+    }
+}
---
+++ src/main/java/TaxiOrderService.java
@@ -0,0 +1,20 @@
+public class TaxiOrderService {
+    private int landingPrice = 60;
+    private int pricePerKm = 20;
+    private int discountPercentage = 5;
+    private int maxDiscount = 100;
+    private int priceForDiscount = 1000;
+
+    int price(int distance) {
+        int totalPrice = distance * pricePerKm + landingPrice;
+        if (totalPrice > priceForDiscount) {
+            double discount = (totalPrice * discountPercentage) / 100;
+            if (discount < maxDiscount) {
+                totalPrice -= discount;
+            } else {
+                totalPrice -= maxDiscount;
+            }
+        }
+        return totalPrice;
+    }
+}
